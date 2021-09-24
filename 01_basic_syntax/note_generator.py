cd24d580f51582181893c10a7482b5d2640d4439
ymsh <ymsh.dct.1804@gmail.com>
2021-09-24 16:02:15 +0900

    update some functions

---
+++ 01_basic_syntax/note_generator.py
@@ -0,0 +1,61 @@
+# coding: utf-8
+
+def note_num_generator():
+    """数列の値を作るジェネレータ
+
+    Yields:
+       int: 数値
+    """
+    n = 0
+    while True:
+        num = n*n + 2*n + 3    # 数列式
+        num = yield num
+        n += 1
+
+
+def main_generator():
+    """ジェネレータが返す値を使って処理を行う
+    """
+    def do_something(num):
+        return (num % 2, num % 3)
+
+    gen = note_num_generator()  # ジェネレータを作る
+    for i in range(1, 10):
+        num = next(gen)  # ジェネレータから次の値を取り出す
+        result = do_something(num)
+        print(i, result)
+
+
+def note_main_gen(n):
+    """main generator
+
+    Args:
+        n (int): sample number
+
+    Yields:
+        multi: yield multiple genre
+    """
+    yield "start"
+    yield from range(n, 0, -1)  # サブイテレータから値を作る
+    yield from "abc"            # サブイテレータから値を作る
+    yield from [10, 20, 30]     # サブイテレータから値を作る
+    yield from note_sub_gen()   # サブジェネレータから値を作る
+    yield "end"
+
+
+def note_sub_gen():
+    """sub genereator
+
+    Yields:
+        str: sample string
+    """
+    yield "X"
+    yield "Y"
+    yield "Z"
+
+
+def note_subgenerator():
+    """サブジェネレータを持つジェネレータを実行する
+    """
+    gen = note_main_gen(3)
+    print(list(gen))
--- 01_basic_syntax/note_introduction.py
+++ 01_basic_syntax/note_introduction.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
-def string_embedded():
-    """fstring
+def note_string_embedded():
+    """文字列への変数埋め込み
     """
     print("Hi, {}".format("hello world"))
     Name = "Bob"
@@ -14,5 +14,187 @@ def string_embedded():
     print(s.format(B, C, A))
 
 
-if __name__ == "__main__":
-    string_embedded()
+def note_string_sample():
+    """文字列操作
+    """
+    s = 'hello world'
+    print(s.title())
+    print(s.capitalize())
+    print(s.find("l"))
+    print(s.find("x"))
+    print(s.rfind("l"))
+    print(s.replace("l", "x"))
+    print(s.strip(" "))
+
+
+def note_for_else_break():
+    """for文のelseブロック
+    """
+    numlist = [3, 4.2, 10, "x", 1, 9]  # 文字列が含まれているlist
+    sum = 0
+
+    for num in numlist:
+        # numが数値ではないときは処理をブレイクする
+        if not isinstance(num, (int, float)):
+            print(num, "数値ではない値が含まれていました。")
+            break  # ブレイクする
+        sum += num
+    else:
+        # breakされなかったときは合計する
+        print("合計", sum)
+
+
+def note_try_expect_finally():
+    """try-expect-finaly文例
+    """
+    num = 0
+    try:
+        value = 120 / num
+        print(value)
+    except Exception as error:
+        print("エラーになりました。")
+        print(error)
+    finally:
+        print("計算終わり。")
+
+
+def note_try_expect_as():
+    """try-expect-as文例
+    """
+    sum = 7600
+    while True:
+        num = input("何人ですか？（qで終了）")
+        if num == "q":
+            print("終了しました。")
+            break
+
+        try:
+            price = round(sum / int(num))
+        except Exception as error:
+            print("エラーになりました。")
+            print(error)
+        else:
+            if price < 0:
+                # マイナスの場合は無視
+                continue
+            print("１人当たりの金額", price)
+
+
+def note_for_enumerate():
+    """enumereateを用いたfor文
+    """
+    names = ["鈴木", "田中", "栗林", "山岡"]
+    for i, who in enumerate(names, 1):
+        print(f"{i}：{who}さん")
+
+
+def note_for_zip():
+    """for文で2つのリストを同時にまわす
+    """
+    name1 = ["鈴木", "田中", "赤尾", "佐々木", "高田"]
+    name2 = ["星奈", "優美", "恵子", "薫花", "幸恵"]
+    longname = []
+
+    # name1とname2を連結したリストを作る
+    for n1, n2 in zip(name1, name2):
+        longname.append(n1+n2)
+    print(longname)
+
+
+def note_list_comprehension():
+    """listの内包表記
+    """
+    numbers = [2.1, 4, "", 2.2, 'x', "12", 3]
+    numbers = [num for num in numbers if isinstance(num, (int, float))]
+    print(numbers)
+
+
+def note_route(start, end, *args):
+    """任意の数の引数を受け取る
+
+    Args:
+        start (str): start place
+        end (str): end place
+        *args: waypoint
+    """
+    route_list = [start]
+    route_list += list(args)
+    route_list += [end]
+
+    route_str = "->".join(route_list)
+    print(route_str)
+
+
+def note_route_args():
+    """route()を試す
+    """
+    start = "東京"
+    end = "宮崎"
+    note_route(start, end, "神戸", "長崎", "熊本")
+
+
+def note_entry(name, gender, **kwargs):
+    """任意の数の引数を受け取る
+
+    Args:
+        name (str): name
+        gender (str): gender
+    """
+    data = {"name": name, "gender": gender}  # 必須の引数の辞書
+    data.update(kwargs)  # 必須の辞書とオプションの辞書を１つに合わせる
+    print(data)
+
+
+def note_route_kwargs():
+    """entry()を試す
+    """
+    note_entry(name="大山坂道", gender="男性", age=27, course="E")
+
+
+def note_size(item):
+    """指定文字列の順序を定義して順序を返す
+
+    Args:
+        item (str): 文字列を割り振られたindexにして返す
+
+    Returns:
+        int: 文字列のindexリスト
+    """
+    sizelist = ["XS", "S", "M", "L"]  # この順に並び替える
+    pos = sizelist.index(item)  # itemのインデックス番号を値として返す
+    return pos
+
+
+def note_sort_size():
+    """note_size関数を用いてdataをサイズ順に並べる
+    """
+    data = ["S", "M", "XS", "L", "M", "M", "XS",  "S", "M", "L", "M"]
+    data.sort(key=note_size)
+    print(data)
+
+
+def note_sort_size_lambda():
+    """note_sort_sizeと同様処理をlambda式で行う
+    """
+    sizelist = ["XS", "S", "M", "L"]   # この順に並び替える
+    data = ["S", "M", "XS", "L", "M", "M", "XS",  "S", "M", "L", "M"]
+    data.sort(key=lambda item: sizelist.index(item))  # dataをサイズ順に並べる
+    print(data)
+
+
+def note_map_double():
+    """map関数を使ってリストの数値を２倍する
+    """
+    def double(x):
+        return x * 2
+    nums = [4, 3, 7, 6, 2, 1]
+    nums2 = list(map(double, nums))
+    print(nums2)
+
+
+def note_filter():
+    """note_map_doubleと同様処理をlambda式で行う
+    """
+    nums = [4, -3, 9, 1, -2, -4, 5]
+    nums = list(filter(lambda x: x > 0, nums))
+    print(nums)
