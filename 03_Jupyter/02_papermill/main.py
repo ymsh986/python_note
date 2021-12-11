c519d95c603dbf4c49534ccbda2f6d3f5af8dc96
ymsh <ymsh.dct.1804@gmail.com>
2021-12-12 07:02:49 +0900

    update papermill
    add .gitignore for ipynb_checkpoints.
    add output sample.
    fix flake8 error.

--- 03_Jupyter/02_papermill/main.py
+++ 03_Jupyter/02_papermill/main.py
@@ -3,7 +3,7 @@ import papermill as pm
 new_beta = [1, 2, 3]
 
 pm.execute_notebook('try_papermill.ipynb',
-                    'output.ipynb',
+                    'try_papermill_output.ipynb',
                     parameters=dict(alpha=['hello', 'world', 1],
                                     beta=new_beta,
                                     gamma={1: 'hello', 2: 'world'},
